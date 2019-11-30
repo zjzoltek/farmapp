3d14d68049556593dc20aed917178dd39b8f263d
Devin Ferguson <devin.ferguson.it@gmail.com>
2019-11-30 15:49:01 -0600

    intial backup
    added intial backup app if electron doesn't work.

---
+++ flask_back_up/farmapp.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request, json
+from flaskext.mysql import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+import yaml
+import os
+
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+# if this file isn't found then use the exampleconfig.yml and copy it to config.yml
+with open(os.path.join(__location__, "config.yml"), 'r') as ymlfile:
+    cfg = yaml.load(ymlfile)
+
+app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = cfg['mysql']['user']
+app.config['MYSQL_DATABASE_PASSWORD'] = cfg['mysql']['passwd']
+app.config['MYSQL_DATABASE_DB'] = cfg['mysql']['db']
+app.config['MYSQL_DATABASE_HOST'] = cfg['mysql']['host']
+mysql.init_app(app)
+
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route('/showSignUp')
+def showSignUp():
+    return render_template('signup.html')
+
+@app.route('/signUp', methods=['POST'])
+def signUp():
+    try:
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _password = request.form['inputPassword']
+
+
+        if _name and _email and _password:
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            _hashed_password = generate_password_hash(_password)
+            cursor.callproc('createUser', (_name, _email))
+            data = cursor.fetchall()
+            if len(data) is 0:
+                conn.commit()
+                return json.dumps({'message':'User created successfully !'})
+            else:
+                return json.dumps({'error':str(data[0])})
+        else:
+            return json.dumps({'html':'<span>Enter the required fields</span>'})
+    except Exception as e:
+        return json.dumps({'error':str(e)})
+    finally:
+        cursor.close() 
+        conn.close()
+
+@app.route('/showSignIn')
+def showSignin():
+    return render_template('signin.html')
+
+@app.route('/validateLogin', methods=['POST'])
+def validateLogin():
+    try:
+        _username = request.form['inputEmail']
+        _password = request.form['inputPassword']
+    # connect to mysql
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('sp_validateLogin',(_username,))
+        data = cursor.fetchall()
+
+        if len(data) > 0:
+            if check_password_hash(str(data[0][3]),_password):
+                session['user'] = data[0][0]
+                return redirect('/userHome')
+            else:
+                return render_template('error.html',error = 'Wrong Email address or Password.')
+        else:
+            return render_template('error.html',error = 'Wrong Email address or Password.')
+ 
+ 
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+    finally:
+        cursor.close()
+        con.close()
+
+@app.route('/userHome')
+def userHome():
+    return render_template('userHome.html')
+
+if __name__ == "__main__":
+    app.run()
