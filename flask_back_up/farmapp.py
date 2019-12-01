4769b416f7a595f2a4bd0178fb34002b2bc68231
Devin Ferguson <devin.ferguson.it@gmail.com>
2019-12-01 12:39:34 -0600

    moved file around

--- flask_back_up/farmapp.py
+++ flask_back_up/farmapp.py
@@ -103,135 +103,168 @@ def logout():
 
 @app.route('/showAllLivestockView')
 def showAllLivestockView():
-    con = mysql.connect()
-    cursor = con.cursor()
-    cursor.callproc('Get_All_Livestock',[1]) # 1 is hard coded
-    data = cursor.fetchall()
-    cursor.close()
-    return render_template('showAllLivestockView.html', livestockdata=data)
-
-@app.route('/add_Livestock', methods=['POST'])
-def add_Livestock():
-    if request.method == 'POST':
-        ownerID = request.form['OwnerID']
-        bornDate = request.form['input_born_date']
-        subType = request.form['input_sub_type']
-        health = request.form['input_health']
-        notes = request.form['input_notes']
-        weight = request.form['input_weight']
-        market_date = request.form['input_market_date']
-        goal = request.form['input_goal_price']
-        sale = request.form['input_sale_price']
-        location = request.form['input_location']
+    if session.get('user'):
         con = mysql.connect()
         cursor = con.cursor()
-        cursor.callproc('Insert_new_livestock_Record',(ownerID, bornDate, subType, health, notes, weight, market_date, goal, sale, location))
-        con.commit()
+        cursor.callproc('Get_All_Livestock',[1]) # 1 is hard coded
+        data = cursor.fetchall()
         cursor.close()
-    return json.dumps({'message':'record created successfully !'})
+        return render_template('showAllLivestockView.html', livestockdata=data)
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
+        
+@app.route('/add_Livestock', methods=['POST'])
+def add_Livestock():
+    if session.get('user'):
+        if request.method == 'POST':
+            ownerID = request.form['OwnerID']
+            bornDate = request.form['input_born_date']
+            subType = request.form['input_sub_type']
+            health = request.form['input_health']
+            notes = request.form['input_notes']
+            weight = request.form['input_weight']
+            market_date = request.form['input_market_date']
+            goal = request.form['input_goal_price']
+            sale = request.form['input_sale_price']
+            location = request.form['input_location']
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('Insert_new_livestock_Record',(ownerID, bornDate, subType, health, notes, weight, market_date, goal, sale, location))
+            con.commit()
+            cursor.close()
+        return json.dumps({'message':'record created successfully !'})
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
 @app.route('/deleteLiverstockRecord/<string:livestockID>', methods=['POST'])
 def deleteLiverstockRecord(livestockID):
-    if request.method == 'POST':
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('Delete_livestock_Record',[livestockID])
-        con.commit()
-        cursor.close()
-    return json.dumps({'message':'record created successfully !'})
+    if session.get('user'):
+        if request.method == 'POST':
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('Delete_livestock_Record',[livestockID])
+            con.commit()
+            cursor.close()
+        return json.dumps({'message':'record created successfully !'})
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
 @app.route('/showMedicalRecords/<string:id>', methods=['GET', 'POST'])
 def showMedicalRecords(id):
-    con = mysql.connect()
-    cursor = con.cursor()
-    cursor.callproc('Get_ALL_Medication_Records',[id])
-    medicationRecord = cursor.fetchall()
-    cursor.callproc('Get_all_Vaccination_Records', [id])
-    vaccinationRecord = cursor.fetchall()
-    cursor.callproc('Get_all_vetvisits_records', [id])
-    vetVisits = cursor.fetchall()
-    cursor.close()
-    return render_template('showMedicalRecords.html', livestockID = id, medRecord = medicationRecord, vaccinRecord = vaccinationRecord, vetRecord = vetVisits)
+    if session.get('user'):
+        con = mysql.connect()
+        cursor = con.cursor()
+        cursor.callproc('Get_ALL_Medication_Records',[id])
+        medicationRecord = cursor.fetchall()
+        cursor.callproc('Get_all_Vaccination_Records', [id])
+        vaccinationRecord = cursor.fetchall()
+        cursor.callproc('Get_all_vetvisits_records', [id])
+        vetVisits = cursor.fetchall()
+        cursor.close()
+        return render_template('showMedicalRecords.html', livestockID = id, medRecord = medicationRecord, vaccinRecord = vaccinationRecord, vetRecord = vetVisits)
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
 @app.route('/add_Medication', methods=['POST'])
 def add_Medication():
-    if request.method == 'POST':
-        livestockID = request.form['livestockID']
-        medicationName = request.form['input_medication_name']
-        start_date = request.form['input_start_date']
-        end_date = request.form['input_end_date']
-        med_interval = request.form['input_medication_interval']
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('Insert_new_Medication_Record',(livestockID, medicationName, start_date, end_date, med_interval))
-        con.commit()
-        cursor.close()
-    return json.dumps({'message':'record created successfully !'})
+    if session.get_id('user'):
+        if request.method == 'POST':
+            livestockID = request.form['livestockID']
+            medicationName = request.form['input_medication_name']
+            start_date = request.form['input_start_date']
+            end_date = request.form['input_end_date']
+            med_interval = request.form['input_medication_interval']
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('Insert_new_Medication_Record',(livestockID, medicationName, start_date, end_date, med_interval))
+            con.commit()
+            cursor.close()
+        return json.dumps({'message':'record created successfully !'})
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
 @app.route('/delete_medicationRecord/<string:medID>', methods=['POST'])
 def delete_medicationRecord(medID):
-    if request.method == 'POST':
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('Delete_Medication_Record',[medID])
-        con.commit()
-        cursor.close()
-    return json.dumps({'message':'record created successfully !'})
+    if session.get_id('user'):
+        if request.method == 'POST':
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('Delete_Medication_Record',[medID])
+            con.commit()
+            cursor.close()
+        return json.dumps({'message':'record created successfully !'})
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
 @app.route('/add_Vaccination', methods=['POST'])
 def add_Vaccination():
-    if request.method == 'POST':
-        livestockID = request.form['livestockID']
-        vaccinType = request.form['input_vaccin_type']
-        vaccinDate = request.form['input_date_given']
-        print(livestockID, vaccinType, vaccinDate)
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('Insert_new_Vaccination_Record', (livestockID, vaccinType, vaccinDate))
-        con.commit()
-        con.close()
-    return json.dumps({'message':'record created successfully !'})
+    if session.get_id('user'):
+        if request.method == 'POST':
+            livestockID = request.form['livestockID']
+            vaccinType = request.form['input_vaccin_type']
+            vaccinDate = request.form['input_date_given']
+            print(livestockID, vaccinType, vaccinDate)
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('Insert_new_Vaccination_Record', (livestockID, vaccinType, vaccinDate))
+            con.commit()
+            con.close()
+        return json.dumps({'message':'record created successfully !'})
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
 @app.route('/delete_vaccineRecord/<string:vaccID>', methods=['POST'])
 def delete_vaccineRecord(vaccID):
-    if request.method == 'POST':
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('Delete_Vaccination_Record',[vaccID])
-        con.commit()
-        cursor.close()
-    return json.dumps({'message':'record created successfully !'})
+    if session.get('user'):
+        if request.method == 'POST':
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('Delete_Vaccination_Record',[vaccID])
+            con.commit()
+            cursor.close()
+        return json.dumps({'message':'record created successfully !'})
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
 @app.route('/add_vetVisit', methods=['POST'])
 def add_vetVisit():
-    if request.method == 'POST':
-        livestockID = request.form['livestockID']
-        visitDate =  request.form['input_visit_date']
-        vetName =  request.form['input_vet_name']
-        cost = request.form['input_visit_cost']
-        reason = request.form['input_visit_reason']
-        notes =  request.form['input_visit_notes']
-        print(livestockID, visitDate, vetName, cost, reason, notes)
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('Insert_new_VetVist_Record',(livestockID, visitDate, vetName, cost, reason, notes))
-        con.commit()
-        con.close()
-    return json.dumps({'message':'record created successfully !'})
+    if session.get('user'):
+        if request.method == 'POST':
+            livestockID = request.form['livestockID']
+            visitDate =  request.form['input_visit_date']
+            vetName =  request.form['input_vet_name']
+            cost = request.form['input_visit_cost']
+            reason = request.form['input_visit_reason']
+            notes =  request.form['input_visit_notes']
+            print(livestockID, visitDate, vetName, cost, reason, notes)
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('Insert_new_VetVist_Record',(livestockID, visitDate, vetName, cost, reason, notes))
+            con.commit()
+            con.close()
+        return json.dumps({'message':'record created successfully !'})
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
 @app.route('/delete_vetRecord/<string:vetID>', methods=['POST'])
 def delete_vetRecord(vetID):
-    if request.method == 'POST':
-        con = mysql.connect()
-        cursor = con.cursor()
-        cursor.callproc('Delete_VetVisit_Record',[vetID])
-        con.commit()
-        cursor.close()
-    return json.dumps({'message':'record created successfully !'})
+    if session.get('user'):
+        if request.method == 'POST':
+            con = mysql.connect()
+            cursor = con.cursor()
+            cursor.callproc('Delete_VetVisit_Record',[vetID])
+            con.commit()
+            cursor.close()
+        return json.dumps({'message':'record created successfully !'})
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
 @app.route('/viewPastures')
 def viewPastures():
-    return render_template('viewPastures.html')
+    if session.get('user'):
+        return render_template('viewPastures.html')
+    else:
+        return render_template('error.html', error = 'Unauthorized Access')
 
 if __name__ == "__main__":
     app.run()
